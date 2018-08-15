cd1e4e3205d37fc11c84767a67d9e3cea93ba24a
kevin.d.tyo@gmail.com <kevin.d.tyo@gmail.com>
2018-08-15 23:59:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 funkyFunction();
 theFunk();
-var theFunk = funkyFunction()()
+var theFunk = funkyFunction()
 
 
 
